2f8dfb3999bf9b5b3e085e88dc7437e3c9fd93e2
FAGBEMY MOUNIROU <fagbemymounirou@gmail.com>
2026-08-27 17:29:21 +0100

    The best answer for this exercice . I get how to convert binary to integer

--- ConvertToBinary1.py
+++ ConvertToBinary1.py
@@ -1,12 +1,9 @@
-def convertBinary(oldnumber):
-    if oldnumber < 0:
-        number = abs(oldnumber)
-        binary = bin(number)[2:]  # Convertir en binaire et supprimer le préfixe "0b"
-        return f"{number} -> {binary} (because {oldnumber} is {bin(number)} in binary)"
-    else:
-        binary = bin(oldnumber)[2:]  # Convertir en binaire et supprimer le préfixe "0b"
-        return f"{oldnumber} -> {binary} because {oldnumber} is 0b{binary} in binary"
+def to_binary(n):
+    return int (bin(abs(n))[2:])
 
-oldnumber = int(input("Enter a negative number: "))
-result = convertBinary(oldnumber)
-print(result)  # Affiche uniquement la chaîne retournée
+def to_bin(n):
+    return int (f'{n:b}')
+
+print(to_binary(5))
+print(to_bin(5))
+
