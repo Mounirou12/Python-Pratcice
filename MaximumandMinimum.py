59fe84cd3696c4d5295561ab0ae0ae2ba7d750fc
FAGBEMY MOUNIROU <fagbemymounirou@gmail.com>
2026-08-28 13:24:28 +0100

    I solve this exercice by using the method mix and max. I have to learn on what every method works on

---
+++ MaximumandMinimum.py
@@ -0,0 +1,9 @@
+def minimum(arr):
+    return min(arr) 
+
+def maximum(arr):
+    return max(arr)
+
+
+print(minimum([1,2,3,4,5,6,7,8,9,10,11,12,13,14,-15,16,17,18,19,20]))
+print(maximum([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]))
